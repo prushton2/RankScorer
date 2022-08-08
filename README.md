ad4059bb8575b1a97205e737c77ac6490c09f65c
prushton2 <peterrushton418@gmail.com>
2022-08-08 10:47:39 -0400

    finished

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # RankScorer
+
+A program to score how accurate a rank is. Looks at how close rach submission is to the position on the answer key, and gives more points for closer answers
+
+## How to use
+* input the answer in the answer key list
+* imput the submission into the submitted answers list
+* update the size of both lists
+* run the program
+
+## Score
+* Score is a number <= 0
+* Higher number is more accurate list
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![allow(non_snake_case)]
+
 fn main() {
-    println!("Hello, world!");
+	let mut score: i32 = 0;
+	let answerKey: [String; 3] = [String::from("Hello"), String::from("World"), String::from("Hi")];
+	
+	let submittedAsnwers: [String; 3] = [String::from("Hello"), String::from("Hi"), String::from("World")];
+
+	for (i, x) in answerKey.iter().enumerate() {
+		//find the index of the element in the sibmitted answers array
+		let index = submittedAsnwers
+							.iter()
+							.position(|z| z == x)
+							.unwrap();
+		score -= (i as i32 - index as i32).abs();		
+	}
+
+	println!("Score: {}", score);
+    
+	
 }
+
